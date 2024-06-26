529474ce32ac84314fa1528865624542043212e5
MihaiPan <108823354+MihaiPan@users.noreply.github.com>
2024-06-27 00:03:00 +0300

    Update insert_location.php

--- WEB/insert_location.php
+++ WEB/insert_location.php
@@ -1,26 +1,20 @@
 <?php
-// Detalii de conectare la baza de date
 $servername = "localhost";
-$username = "u198184306_stm32_data";
-$password = "Aprozar-12";
-$dbname = "u198184306_stm32_data";
+$username = "";
+$password = "";
+$dbname = "";
 
-// Creează conexiunea la baza de date
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verifică conexiunea
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Verifică dacă datele sunt trimise prin metoda POST
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $latitude = $_POST['latitude'] ?? null;
     $longitude = $_POST['longitude'] ?? null;
 
-    // Verifică dacă toate variabilele necesare sunt setate
     if (isset($latitude, $longitude)) {
-        // Pregătește și execută interogarea SQL pentru inserarea datelor
         $sql = "INSERT INTO location (latitude, longitude) 
                 VALUES ('$latitude', '$longitude')";
 
@@ -36,6 +30,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     echo "No data received";
 }
 
-// Închide conexiunea
 $conn->close();
 ?>
