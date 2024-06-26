8ba88746a33efab0ba8c953ae7beb24b92fb4375
MihaiPan <108823354+MihaiPan@users.noreply.github.com>
2024-06-27 00:00:22 +0300

    Update get_latest_location.php

--- WEB/get_latest_location.php
+++ WEB/get_latest_location.php
@@ -1,24 +1,20 @@
 <?php
-// Detalii de conectare la baza de date
+
 $servername = "localhost";
 $username = "";
 $password = ""; 
 $dbname = "";
 
-// Creează conexiunea la baza de date
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verifică conexiunea
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Execută interogarea pentru a extrage ultima înregistrare
 $sql = "SELECT * FROM location ORDER BY id DESC LIMIT 1";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    // Datele au fost găsite, le returnăm în format JSON
     $row = $result->fetch_assoc();
     echo json_encode($row);
 } else {
